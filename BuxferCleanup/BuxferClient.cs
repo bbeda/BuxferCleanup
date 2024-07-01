e7f148feb1cd9f19c63f10aaed1e6f52aba7caad
bbeda <bogdan.beda@force24.co.uk>
2024-07-02 00:15:24 +0300

    Add project files.

---
+++ BuxferCleanup/BuxferClient.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Json;
+
+namespace BuxferCleanup;
+internal class BuxferClient(
+    IHttpClientFactory httpClientFactory,
+    IOptions<BuxferOptions> buxferOptions,
+    IMemoryCache memoryCache)
+{
+    private const string BuxferTokenKey = "BuxferToken";
+
+    public const string BuxferClientName = "Buxfer";
+
+    private async Task<string> GetTokenAsync()
+    {
+        using var client = CreateHttpClient();
+        var response = await client.GetAsync($"login?email={buxferOptions.Value.Email}&password={buxferOptions.Value.Password}");
+        _ = response.EnsureSuccessStatusCode();
+        var tokenResponse = await response.Content.ReadFromJsonAsync<BuxferResponse<TokenResponse>>();
+        return tokenResponse?.Response.Token!;
+    }
+
+    private async Task<string> LoadTokenAsync()
+    {
+        if (memoryCache.TryGetValue(BuxferTokenKey, out string cachedToken))
+        {
+            return cachedToken!;
+        }
+
+        var token = await GetTokenAsync();
+        if (string.IsNullOrWhiteSpace(token))
+        {
+            throw new InvalidOperationException("Failed to get token from Buxfer");
+        }
+
+        memoryCache.Set(BuxferTokenKey, token, TimeSpan.FromHours(5));
+
+        return token;
+    }
+
+    public async Task<TransactionsListResponse> LoadTransactionsAsync(string accountId)
+    {
+        var token = await LoadTokenAsync();
+        using var client = CreateHttpClient();
+        var response = await client.GetAsync($"transactions?token={token}&page=1&accountId={accountId}");
+        return (await response!.Content!.ReadFromJsonAsync<BuxferResponse<TransactionsListResponse>>())?.Response!;
+    }
+
+    private HttpClient CreateHttpClient() => httpClientFactory.CreateClient(BuxferClientName);
+
+    private record TokenResponse(string Status, string Token);
+
+    private record BuxferResponse<T>
+    {
+        public T Response { get; init; } = default!;
+    }
+}
+
+internal record BuxferOptions
+{
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+}
+
+internal record BuxferTransaction(
+    decimal Id,
+    string Description,
+    decimal Amount,
+    string Date,
+    string Type,
+    decimal AccountId,
+    string Tags);
+
+internal record TransactionsListResponse(
+    string Status,
+    int NumTransactions,
+    BuxferTransaction[] Transactions);
---
+++ BuxferCleanup/Program.cs
@@ -0,0 +1,26 @@
+﻿using BuxferCleanup;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder().ConfigureAppConfiguration(c => c.AddUserSecrets<BuxferClient>()).ConfigureServices((hostContext, services) =>
+{
+    _ = services.AddHttpClient(BuxferClient.BuxferClientName, client =>
+    {
+        client.BaseAddress = new Uri("https://www.buxfer.com/api/");
+    });
+    _ = services.AddMemoryCache();
+    _ = services.Configure<BuxferOptions>(hostContext.Configuration);
+
+    _ = services.AddSingleton<BuxferClient>();
+}).UseConsoleLifetime()
+.Build();
+
+var buxferClient = host.Services.GetRequiredService<BuxferClient>();
+
+var transactions = await buxferClient.LoadTransactionsAsync("1441844");
+
+foreach (var transaction in transactions.Transactions)
+{
+    Console.WriteLine($"{transaction.Id} - {transaction.AccountId} - {transaction.Date} - {transaction.Amount} - {transaction.Description}");
+}
